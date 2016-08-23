be06322e3dd63126d6eeb2e4db9acf732d9b0ea8
Erick Nilsen Pereira de Souza - DATAPREVCE <erick.nilsen@dataprev.gov.br>
2016-08-23 09:57:54 -0300

    exemplo layer

---
+++ LayerExemplo/src/br/unifor/view/MeuEditor.java
@@ -0,0 +1,60 @@
+package br.unifor.view;
+import java.awt.BorderLayout;
+import java.awt.Container;
+
+import javax.swing.Action;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+public class MeuEditor extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JMenuBar menubar = new JMenuBar();
+	private JMenu menu = new JMenu("Arquivo");
+	private JTextArea texto = new JTextArea();
+
+	//define o evento de salvamento do arquivo	
+	private Action salvar = new SalvarAction(this.texto);
+
+	//construtor: pede uma janela ao SO, cria um menu e uma área para escrita
+	public MeuEditor() {
+		super("Meu Editor");
+
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container interno = this.getContentPane();
+		this.montaMenu();
+	
+		this.montaGUI(interno);
+	}
+
+	//constrói um menu com um item para salvar o arquivo
+	private void montaMenu() {
+
+		JMenuItem itemSalvar = new JMenuItem(this.salvar);
+
+		this.menu.add(itemSalvar);
+
+		this.menubar.add(this.menu);
+		this.setJMenuBar(this.menubar);
+	}
+
+	//define o layout e insere um textarea no container principal da janela
+	private void montaGUI(Container interno) {
+		interno.setLayout(new BorderLayout());		
+		interno.add(new JScrollPane(this.texto));
+	}
+
+	//método principal: instancia a classe, define as dimensões da janela e sua visibilidade	
+	public static void main(String[] args) {
+		MeuEditor janela = new MeuEditor();
+		janela.setSize(640, 480);
+		janela.setVisible(true);		
+	}
+}
---
+++ LayerExemplo/src/br/unifor/view/SalvarAction.java
@@ -0,0 +1,47 @@
+package br.unifor.view;
+import java.awt.event.ActionEvent;
+import java.io.File;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JFileChooser;
+import javax.swing.JTextArea;
+
+import br.unifor.business.EditorBS;
+import br.unifor.vo.ArquivoTexto;
+
+public class SalvarAction extends AbstractAction {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JTextArea texto;
+
+	//construtor: recebe o texto inserido pelo usuário no textarea da janela
+	public SalvarAction(JTextArea texto) {		
+		super("Salvar");	
+		this.texto = texto;
+		this.putValue(Action.SHORT_DESCRIPTION, "Salva arquivo texto");		
+	}
+	
+	//captura a ação de clique no item de menu
+	public void actionPerformed(ActionEvent ev) {
+		//cria a tela de diálogo para salvamento do arquivo
+		JFileChooser jfc = new JFileChooser();
+		int resp = jfc.showSaveDialog(this.texto);
+		if (resp != JFileChooser.APPROVE_OPTION)
+			return;
+
+		//seleciona o arquivo de salvamento
+		File arquivo = jfc.getSelectedFile();
+		
+		ArquivoTexto arquivoTexto = new ArquivoTexto();
+		arquivoTexto.setArquivo(arquivo);
+		arquivoTexto.setConteudo(this.texto.getText());
+		
+		EditorBS editorBS = new EditorBS(arquivoTexto);
+		editorBS.salvarArquivo();								
+	}
+
+}
