ba7a64d5b8e16a235321dadfb306d91b04b03a94
Erick Nilsen Pereira de Souza - DATAPREVCE <erick.nilsen@dataprev.gov.br>
2016-10-03 12:30:11 -0300

    SPA exemplo

--- SPA_Exemplo/back-end/ExemploRest/src/br/unifor/restful/resources/EmpregadoResource.java
+++ SPA_Exemplo/back-end/ExemploRest/src/br/unifor/restful/resources/EmpregadoResource.java
@@ -1,9 +1,10 @@
 package br.unifor.restful.resources;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
-import javax.ejb.EJB;
-import javax.ejb.Stateless;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -13,54 +14,76 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
-import br.unifor.bean.EmpregadoBeanRemote;
+import br.unifor.restful.model.DepartamentoVO;
 import br.unifor.restful.model.EmpregadoVO;
-import br.unifor.restful.util.MontaObjetos;
+import br.unifor.restful.model.EstacionamentoVO;
+import br.unifor.restful.model.ProjetoVO;
 
-
-@Stateless
 @Path("/empregados")
 public class EmpregadoResource {
 
-    @EJB private EmpregadoBeanRemote empregadoBean;
+	static private Map<Integer, EmpregadoVO> empregadosMap;
 
-    @GET
-    @Produces("application/json")
-    public Collection<EmpregadoVO> listaProjetos() {              
+	static {
+		empregadosMap = new HashMap<Integer, EmpregadoVO>();
 
-        return MontaObjetos.montaEmpregadosVO(empregadoBean.lista());         
+		EmpregadoVO e1 = new EmpregadoVO();
+		e1.setId(1L);
+		e1.setNome("Maria da Silva");
+		e1.setEmail("maria@mail.com");
+		e1.setDepartamento(new DepartamentoVO());
+		e1.setProjetos(new ArrayList<ProjetoVO>());
+		e1.setEstacionamento(new EstacionamentoVO());
+		empregadosMap.put(e1.getId().intValue(), e1);
 
-    }
-    
-    @GET
+		EmpregadoVO e2 = new EmpregadoVO();
+		e2.setId(2L);
+		e2.setNome("João dos Santos");
+		e2.setEmail("joao@mail.com");		
+		e2.setDepartamento(new DepartamentoVO());
+		e2.setProjetos(new ArrayList<ProjetoVO>());
+		e2.setEstacionamento(new EstacionamentoVO());
+		empregadosMap.put(e2.getId().intValue(), e2);
+
+	}
+
+	@GET
+	@Produces("application/json")
+	public Collection<EmpregadoVO> lista() {
+		return new ArrayList<EmpregadoVO>(empregadosMap.values());
+	}
+
+	@GET
 	@Path("{id}")
-    @Produces("application/json")
-	public EmpregadoVO buscaProjetoPorId(@PathParam("id") Long id) {
-    	return MontaObjetos.montaEmpregadoVO(empregadoBean.busca(id));
-    }
+	@Produces("application/json")
+	public EmpregadoVO busca(@PathParam("id") Long id) {
+		System.out.println("ID: "+id);		
+		return empregadosMap.get(id.intValue());
+	}
 
+	@POST
+	@Consumes("application/json")
+	@Produces("text/plain")
+	public String insere(EmpregadoVO empregadoVO) {
+		empregadoVO.setId(empregadosMap.size() + 1L);
+		empregadosMap.put(empregadoVO.getId().intValue(), empregadoVO);
+		return "Empregado " + empregadoVO.getNome() + " adicionado.";
 
-    @POST
-    @Consumes("application/json")
-    @Produces("text/plain")
-    public String adicionaProjeto(EmpregadoVO empregadoVO) {
+	}
 
-        return empregadoBean.adiciona(MontaObjetos.montaEmpregado(empregadoVO));
-    }   
+	@PUT
+	@Consumes("application/json")
+	@Produces("text/plain")
+	public String altera(EmpregadoVO empregadoVO) {
 
-    @PUT
-    @Consumes("application/json")
-    @Produces("text/plain")
-    public String alteraProjeto(EmpregadoVO empregadoVO) {        
+		return null;
+	}
 
-        return empregadoBean.altera(MontaObjetos.montaEmpregado(empregadoVO));
-    }
-
-    @Path("{id}")
-    @DELETE 
-    @Produces("text/plain")
-    public String removeProjeto(@PathParam("id") Long id) {       
-        return empregadoBean.remove(id);
-    }   
+	@Path("{id}")
+	@DELETE
+	@Produces("text/plain")
+	public String remove(@PathParam("id") Long id) {
+		return null;
+	}
 
 }
